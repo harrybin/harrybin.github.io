de93ea84df1dabd398f1810c92705c467f535f7a
Harald Binkle <15869006+harrybin@users.noreply.github.com>
2025-01-21 07:55:13 +0100

    page setup update

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Harald Binkle",
   ogImage: "harrybin_icon.png",
   lightAndDarkMode: false,
-  postPerPage: 5,
+  postPerPage: 10,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
